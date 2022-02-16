e257b5e2357d2fca440f81669bb51158a6d2ebe2
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-16 22:43:37 +0100

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/maycolmohrcursomc/services/CategoriaService.java
+++ src/main/java/com/maycolmohrcursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.maycolmohrcursomc.entities.Categoria;
 import com.maycolmohrcursomc.repositories.CategoriaRepository;
+import com.maycolmohrcursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
-
 }
